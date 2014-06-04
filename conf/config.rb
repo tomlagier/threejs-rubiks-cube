33995a1be20606e1428cab0c49938a8c568103be
choage <iam@chrishoage.com>
2014-06-04 10:23:08 -0700

    fixed paths for realitive sprites

--- conf/config.rb
+++ conf/config.rb
@@ -1,8 +1,8 @@
 fonts_dir= "src/assets/fonts"
 http_fonts_dir="assets/fonts"
 images_dir = "src/assets/images"
-generated_images_dir = "assets/images"
-http_images_path = http_generated_images_path = "/assets/images"
+generated_images_dir = "target/assets/css/sprites"
+http_images_path = http_generated_images_path = "sprites"
 css_dir = "target/assets/css"
 output_style = (environment == :production) ? :compressed : :expanded
 preferred_syntax = :scss
