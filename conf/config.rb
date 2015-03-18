cd13dac1504ebc4a927f5b809f2b6082d1f4b1c7
chrishoage <iam@chrishoage.com>
2015-03-18 14:22:03 -0700

    updated build tools, removed legacy conf folder, added build script

--- conf/config.rb
+++
@@ -1,9 +0,0 @@
-fonts_dir= "src/assets/fonts"
-http_fonts_dir="assets/fonts"
-images_dir = "src/assets/images"
-generated_images_dir = "target/assets/css/sprites"
-http_images_path = http_generated_images_path = "sprites"
-css_dir = "target/assets/css"
-output_style = (environment == :production) ? :compressed : :expanded
-preferred_syntax = :scss
-sass_dir = "src/assets/scss"
