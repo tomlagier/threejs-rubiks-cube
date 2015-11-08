827b76ac990e9f383b741c48b19f1a56eff688f7
Tom Lagier <tom@rosetta.com>
2015-11-07 17:22:57 -0800

    Build pathing

--- build/build.sh
+++ build/build.sh
@@ -1,21 +1,21 @@
-# Clean up images
+##CWD is ./
 REPODIR=${PWD%.*}
 PROJECT=${REPODIR##*/}
 
-mv build/Dockerfile-build .
+mv build/Dockerfile-build ../
 docker kill $PROJECT-build
 docker rm $PROJECT-build
-docker build -t tomlagier/$PROJECT-build -f Dockerfile-build .
+docker build -t tomlagier/$PROJECT-build -f ../Dockerfile-build ../
 docker run -d --name $PROJECT-build tomlagier/$PROJECT-build /bin/sh/
-docker cp $PROJECT-build:/build/build.tar.gz ./build/
+docker cp $PROJECT-build:/build/build.tar.gz ./
 docker kill $PROJECT-build
 docker rm $PROJECT-build
 docker kill $PROJECT-host
 docker rm $PROJECT-host
-docker build -t tomlagier/$PROJECT-host -f ./build/Dockerfile-host ./build/
+docker build -t tomlagier/$PROJECT-host -f Dockerfile-host ./
 HOST_PORT=$(jq -r .hostPort package.json)
 docker run -d -p $HOST_PORT:80 --name $PROJECT-host tomlagier/$PROJECT-host
-rm build/build.tar.gz
+rm build.tar.gz
 
 #Clean up images
 docker rmi $(docker images -q)
